e20d7885c0a93bd15afa325df019d8fd5dda5e52
Steven Pham <steven.pham.8.2000@gmail.com>
2022-12-25 07:28:49 -0500

    Refactor goal request handling to allow for multiple events to occur due to a request

--- goals/src/request.rs
+++ goals/src/request.rs
@@ -40,68 +40,71 @@ pub enum GoalRequest {
 }
 
 pub trait GoalRequestHandler {
-    fn handle_request(&mut self, request: GoalRequest) -> Option<GoalEvent>;
+    fn handle_request(&mut self, request: GoalRequest) -> Vec<GoalEvent>;
 }
 
 #[allow(unused_variables)]
-impl<'a> GoalRequestHandler for ProfileAndDateTime<'a> {
-    fn handle_request(&mut self, request: GoalRequest) -> Option<GoalEvent> {
+impl GoalRequestHandler for ProfileAndDateTime<'_> {
+    fn handle_request(&mut self, request: GoalRequest) -> Vec<GoalEvent> {
         match request {
             GoalRequest::AddEffort { goal_id, effort } => {
-                self.0.goals.get_mut(&goal_id).map(|goal| {
+                self.0.goals.get_mut(&goal_id).map_or(vec![], |goal| {
                     goal.add_effort(effort);
 
-                    GoalEvent::Add { goal_id }
+                    vec![GoalEvent::Add { goal_id }]
                 })
             }
             GoalRequest::Focus(goal_id) => {
                 let focused_ids = self.0.focus_goal(goal_id);
 
-                focused_ids.map(|mut focused_ids| {
+                focused_ids.map_or(vec![], |mut focused_ids| {
                     focused_ids.remove(&goal_id);
-                    GoalEvent::Focus {
+                    vec![GoalEvent::Focus {
                         focus_root_id: goal_id,
                         focused_children: focused_ids,
-                    }
+                    }]
                 })
             }
             GoalRequest::Unfocus(goal_id) => {
                 let unfocused_ids = self.0.unfocus_goal(goal_id);
 
-                unfocused_ids.map(|mut focused_ids| {
+                unfocused_ids.map_or(vec![], |mut focused_ids| {
                     focused_ids.remove(&goal_id);
-                    GoalEvent::Unfocus {
+                    vec![GoalEvent::Unfocus {
                         unfocus_root_id: goal_id,
                         unfocused_children: focused_ids,
-                    }
+                    }]
                 })
             }
             GoalRequest::FocusSingle(goal_id) => self
                 .0
                 .focus_single_goal(goal_id)
                 .then_some(goal_id)
-                .map(GoalEvent::FocusSingle),
+                .map_or(vec![], |goal_id| vec![GoalEvent::FocusSingle(goal_id)]),
             GoalRequest::UnfocusSingle(goal_id) => self
                 .0
                 .unfocus_single_goal(goal_id)
                 .then_some(goal_id)
-                .map(GoalEvent::FocusSingle),
+                .map_or(vec![], |goal_id| vec![GoalEvent::UnfocusSingle(goal_id)]),
             GoalRequest::Rescope {
                 goal_id,
                 new_effort_to_complete,
-            } => self.0.rescope_goal(goal_id, new_effort_to_complete).map(
-                |original_effort_to_complete| GoalEvent::Rescope {
-                    goal_id,
-                    new_effort_to_complete,
-                    original_effort_to_complete,
+            } => self.0.rescope_goal(goal_id, new_effort_to_complete).map_or(
+                vec![],
+                |original_effort_to_complete| {
+                    vec![GoalEvent::Rescope {
+                        goal_id,
+                        new_effort_to_complete,
+                        original_effort_to_complete,
+                    }]
                 },
             ),
             GoalRequest::Add {
                 name,
                 effort_to_complete,
-            } => Some(GoalEvent::Add {
+            } => vec![GoalEvent::Add {
                 goal_id: self.0.add_goal(Goal::new(name, effort_to_complete)),
-            }),
+            }],
             GoalRequest::Refine {
                 parent_goal_id,
                 parent_effort_removed,
@@ -114,19 +117,25 @@ impl<'a> GoalRequestHandler for ProfileAndDateTime<'a> {
                     parent_goal_id,
                     parent_effort_removed,
                 )
-                .map(|child_goal_id| GoalEvent::Refine {
-                    parent_goal_id,
-                    parent_effort_removed,
-                    new_child_goal_id: child_goal_id,
+                .map_or(vec![], |child_goal_id| {
+                    vec![GoalEvent::Refine {
+                        parent_goal_id,
+                        parent_effort_removed,
+                        new_child_goal_id: child_goal_id,
+                    }]
                 }),
             GoalRequest::Delete(goal_id) => self
                 .0
                 .remove_goal(goal_id)
-                .map(|deleted_goal_tree| GoalEvent::Delete { deleted_goal_tree }),
+                .map_or(vec![], |deleted_goal_tree| {
+                    vec![GoalEvent::Delete { deleted_goal_tree }]
+                }),
             GoalRequest::Rename { goal_id, new_name } => self
                 .0
                 .rename_goal(goal_id, &new_name)
-                .map(|old_name| GoalEvent::Rename { goal_id, old_name }),
+                .map_or(vec![], |old_name| {
+                    vec![GoalEvent::Rename { goal_id, old_name }]
+                }),
             GoalRequest::ProcessDateTime { datetime } => todo!(),
         }
     }
